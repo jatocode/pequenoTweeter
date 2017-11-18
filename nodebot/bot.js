c07e79942899add311687d95f95063b9dde9d84f
Tobias Jansson <tobias.jansson@gmail.com>
2017-11-18 12:15:48 +0100

    Uptime and IP in words

--- nodebot/bot.js
+++ nodebot/bot.js
@@ -1,7 +1,10 @@
-var twit = require('twit');
-var config = require('./config.js');
+const twit = require('twit');
+const config = require('./config.js');
+const http = require('http');
+const fetch = require('node-fetch');
+const towords = require('number-to-words');
 
-var Twitter = new twit(config);
+const Twitter = new twit(config);
 
 /*
 Twitter.post('statuses/update', { status: 'hello world!' }, function(err, data, response) {
@@ -9,3 +12,34 @@ Twitter.post('statuses/update', { status: 'hello world!' }, function(err, data, 
 })
 */
 
+
+getExternalIP().then((data) => {
+      console.log(data);
+      console.log(getUptime().then((data) => {
+            console.log(data);
+      }));
+});
+
+function getUptime() {
+      const child = require('child_process');
+      return new Promise((resolve, reject) => {
+            child.exec('uptime', function (error, stdout, stderr) {
+                  resolve(stdout);
+            });
+      });
+}
+
+function getExternalIP() {
+      return fetch('https://api.ipify.org?format=json')
+            .then(function (res) {
+                  return res.json();
+            }).then(function (json) {
+                  const a = json.ip.split('.');
+                  var str = '';
+                  a.forEach(function (e) {
+                        str += towords.toWords(e) + '.';
+                  });
+                  return str;
+                  //console.log(json.ip + ' = ' + str);
+            });
+}
